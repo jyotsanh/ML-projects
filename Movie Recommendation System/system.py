9757b9a62cdd45ac40bd8c82c1f4fb8e32648324
Jyotsan HAMAL <hamaljyotsan@gmail.com>
2024-02-17 18:44:19 +0545

    updated recommendation

--- Movie Recommendation System/system.py
+++ Movie Recommendation System/system.py
@@ -1,48 +1,63 @@
-import streamlit as st 
-import pandas as pd 
+from flask import Flask, render_template, request
 import pickle
+import pandas as pd
 import requests
 
-def posture(movie):
-    response = requests.get(f'https://api.themoviedb.org/3/movie/{movie}?api_key=201dadbac9d90432aa44713682a9eb60')
-    path = response.json()
-    poster_path = path['poster_path']
-    return "https://image.tmdb.org/t/p/w500/"+poster_path
+app = Flask(__name__)
 
 
-dice = pickle.load(open('movies.pkl','rb'))
-similarity = pickle.load(open('similar.pkl','rb'))
-df = pd.DataFrame(dice)
 
-#API key = 201dadbac9d90432aa44713682a9eb60
+def fetch_poster(movie_id):
+    response = requests.get(f'https://api.themoviedb.org/3/movie/{movie_id}?api_key=201dadbac9d90432aa44713682a9eb60&language=en-US')
+    data = response.json()
+    return 'https://image.tmdb.org/t/p/w500/'+data['poster_path']
+
+
+
+movies_list = pickle.load(open('movie_dict.pkl','rb'))
+movies = pd.DataFrame(movies_list)
+
+similarity = pickle.load(open('similarity.pkl','rb'))
+
+
 
 
 def recommend(movie):
-    OK = []
-    id = []
-    index = df[df['title']==movie].index[0]
-    
-    distance = similarity[index]
-    movies_list=sorted(list(enumerate(distance)),reverse=True,key=lambda x:x[1])[1:9]
+    movie_index = movies[movies['title']== movie].index[0]
+    distances = similarity[movie_index]
+    movies_list = sorted(list(enumerate(distances)),reverse=True,key=lambda x:x[1])[1:6]
+
+    recommend_movies = []
+    recommend_movies_poster = []
+
     for i in movies_list:
-        OK.append( df.iloc[ i[0] ]['title'] )
-        id.append( df.iloc[ i[0] ]['id'] )
-    return OK,id
-
-def main():
-    st.header("Exopy")
-    choice = st.selectbox("Enter Movie Name ",
-                        df['title'])
-
-    clicked = st.button("Recommend")
-
-    if clicked:
-        name,id = recommend(choice)
-        for i in range(len(name)):
-            st.write(name[i])
-            st.image(posture(id[i]))
+        movie_id = movies.iloc[i[0]].id
+        #fetch the movie poster
+        recommend_movies.append(movies.iloc[i[0]].title)
+        #fetches the poster from API
+        recommend_movies_poster.append(fetch_poster(movie_id))
+    return recommend_movies,recommend_movies_poster
 
 
 
+@app.route('/')
+def index():
+    return render_template('index.html',all = movies['title'].values)
 
-main()
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    movie_name = request.form['movie']
+    recommend_movies,recommend_movies_poster = recommend(movie_name)
+    obj = {
+        'movie':recommend_movies,
+        'poster':recommend_movies_poster
+        
+    }
+
+    return render_template('index.html', obj=recommend_movies)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
